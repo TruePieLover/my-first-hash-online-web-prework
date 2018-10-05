202019e4b7933b02e4a1db8627fadd7354e166eb
Brandon Teich <brandonteich123@gmail.com>
2018-10-05 23:31:13 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,11 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
     my_hash = Hash.new { |hash, key| value }
-    months = Hash.new( "month" )
-months = {"1" => "January", "2" => "February"}
-
-keys = months.keys
-puts "#{keys}"
+    my_hash = 
   
 end
 
