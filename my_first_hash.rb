bb774802861b2685d7cf09f2daa1ddc4ff8a2c3e
Brandon Teich <brandonteich123@gmail.com>
2018-10-05 23:32:28 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-    my_hash = Hash.new { |hash, key| value }
-    keys =  my_hash.keys
-    puts "#{keys}"
+   a_hash = {"name" => "Kody", "sex" => "male"}
   
 end
 
